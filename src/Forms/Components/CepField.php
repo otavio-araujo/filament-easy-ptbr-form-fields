d361f49502ccaaead095cbe817b2f916b508e722
Otávio Araújo <otavio_araujo@hotmail.com>
2025-07-18 01:41:55 -0300

    Enhance `CepField` component with dynamic mapping for address fields, integrate HTTP request for CEP lookup, and add success notification.

--- src/Forms/Components/CepField.php
+++ src/Forms/Components/CepField.php
@@ -4,8 +4,10 @@ namespace OtavioAraujo\FilamentEasyPtbrFormFields\Forms\Components;
 
 use Filament\Actions\Action;
 use Filament\Forms\Components\TextInput;
+use Filament\Notifications\Notification;
 use Filament\Schemas\Components\Component;
 use Filament\Schemas\Components\Utilities\Set;
+use Illuminate\Support\Facades\Http;
 use Livewire\Component as Livewire;
 
 class CepField extends TextInput
@@ -16,12 +18,31 @@ class CepField extends TextInput
 
     private bool $actionLabelHidden = false;
 
+    private string $streetField = 'street';
+
+    private string $neighborhoodField = 'neighborhood';
+
+    private string $cityField = 'city';
+
+    private string $stateField = 'state';
+
+    private string $stateCodeField = 'state_code';
+
+    private string $ibgeCodeField = 'ibge_code';
+
+    private string $countryField = 'country';
+
+    private string $countryCodeField = 'country_code';
+
     protected function setUp(): void
     {
         parent::setUp();
         $this
             ->mask('99999-999')
             ->minLength(9)
+            ->afterStateUpdated(function ($state, Livewire $livewire, Set $set, Component $component) {
+                $this->getCep($state, $set);
+            })
             ->suffixAction(function () {
                 if ($this->actionPosition === 'suffix') {
                     return Action::make('search-action-' . $this->getKey())
@@ -29,7 +50,7 @@ class CepField extends TextInput
                         ->hiddenLabel($this->actionLabelHidden)
                         ->icon('heroicon-o-magnifying-glass')
                         ->action(function ($state, Livewire $livewire, Set $set, Component $component) {
-                            $this->getCep($state);
+                            $this->getCep($state, $set);
                         });
                     //                        ->cancelParentActions();
                 }
@@ -45,8 +66,77 @@ class CepField extends TextInput
         return $this;
     }
 
+    public function setStreetField(string $name): static
+    {
+        $this->streetField = $name;
+
+        return $this;
+    }
+
+    public function setNeighborhoodField(string $name): static
+    {
+        $this->neighborhoodField = $name;
+
+        return $this;
+    }
+
+    public function setCityField(string $name): static
+    {
+        $this->cityField = $name;
+
+        return $this;
+    }
+
+    public function setStateField(string $name): static
+    {
+        $this->stateField = $name;
+
+        return $this;
+    }
+
+    public function setStateCodeField(string $name): static
+    {
+        $this->stateCodeField = $name;
+
+        return $this;
+    }
+
+    public function setIbgeCodeField(string $name): static
+    {
+        $this->ibgeCodeField = $name;
+
+        return $this;
+    }
+
+    public function setCountryField(string $name): static
+    {
+        $this->countryField = $name;
+
+        return $this;
+    }
+
+    public function setCountryCodeField(string $name): static
+    {
+        $this->countryCodeField = $name;
+
+        return $this;
+    }
+
     private function getCep($state, $set): void
     {
-        $set('street', 'Rua');
+        $response = Http::get('https://viacep.com.br/ws/' . $state . '/json/')->json();
+        $set($this->streetField, $response['logradouro']);
+        $set($this->neighborhoodField, $response['bairro']);
+        $set($this->cityField, $response['localidade']);
+        $set($this->stateField, $response['estado']);
+        $set($this->stateCodeField, $response['uf']);
+        $set($this->ibgeCodeField, $response['ibge']);
+        $set($this->countryField, 'Brasil');
+        $set($this->countryCodeField, 'BR');
+        Notification::make()
+            ->success()
+            ->title('CEP Encontrado')
+            ->body('CEP: ' . $state)
+            ->send();
     }
 }
